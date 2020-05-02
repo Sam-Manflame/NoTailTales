87f1d077f466a1472f9caf0aa0aec7e4e7edcfdf
Sergey Savchuk <sleepingdog.dev@gmail.com>
2020-05-02 13:07:53 +0300

    Создал и настроил сцену главного меню

---
+++ Assets/Scripts/MainMenuScript.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuScript : MonoBehaviour
+{
+    public int gameScene;
+    public int settingsScene;
+    public int creditsScene;
+
+    private void Start()
+    {
+        // TODO: change NEW GAME to CONTINUE
+    }
+
+    public void newGamePressed()
+    {
+        SceneManager.LoadScene(gameScene);
+    }
+
+    public void settingsPressed()
+    {
+        SceneManager.LoadScene(settingsScene);
+    }
+
+    public void creditsPressed()
+    {
+        SceneManager.LoadScene(creditsScene);
+    }
+
+    public void exitPressed()
+    {
+        Application.Quit();
+    }
+}
---
+++ Assets/Scripts/MovableElement.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class MovableElement : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnMouseOver()
+    {
+        Debug.Log("over");
+    }
+
+    void OnMouseDown()
+    {
+        Debug.Log("down");
+    }
+}
